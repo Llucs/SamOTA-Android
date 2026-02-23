166de8ceaa8d35dcf8b50153436ce924c7aa3b97
Llucs <104036495+Llucs@users.noreply.github.com>
2026-02-22 22:27:48 -0300

    Add files via upload

--- app/src/main/java/com/llucs/samota/ui/screens/DownloadScreen.kt
+++ app/src/main/java/com/llucs/samota/ui/screens/DownloadScreen.kt
@@ -31,12 +31,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import kotlin.math.roundToInt
+import androidx.compose.foundation.text.KeyboardOptions
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
